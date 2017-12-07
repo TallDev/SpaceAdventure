6e491ab7cd70a25708db32df32a6f31e948cdc87
Tallerik <tallerik3@gmail.com>
2017-12-07 18:54:53 +0100

    Test Commit

--- src/SpaceAdventure/Main/SchrottKollision.java
+++ src/SpaceAdventure/Main/SchrottKollision.java
@@ -7,6 +7,7 @@ public class SchrottKollision {
 
     public SchrottKollision() {
         Timer timer;
+
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
